54efa010e9bc08fb1d5f2e8ad9a6cdf238968709
Ankit Solanki <ankit03june@gmail.com>
2018-08-13 18:35:39 +0530

    [DONE] data processing and collecting dialogues from all seasons

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,14 +1,13 @@
-import re
-import glob
+import re, os
+import glob, json
 
 class Process:
     def __init__(self):
+        self.dialogues = {'Rick':[], 'Morty':[]}
 
-    def form_dialogues(self):
-        with open('dataset/season1.txt', 'r') as f:
+    def get_dialogues(self, file):
+        with open(file, 'r') as f:
             lines = f.readlines()
-
-        dialogues = {'Rick':[], 'Morty':[]}
 
         for l in lines:
             lsplit = l.split(':', 1)
@@ -18,12 +17,35 @@ class Process:
 
                 # remove the situation description
                 lsplit[1] = re.sub("[\(\[].*?[\)\]]", "", lsplit[1])
-                if lsplit[0] in dialogues:
-                    dialogues[lsplit[0]].append(lsplit[1])
+                if lsplit[0] in self.dialogues:
+                    self.dialogues[lsplit[0]].append(lsplit[1])
                 else:
-                    dialogues[lsplit[0]] = [lsplit[1]]
+                    self.dialogues[lsplit[0]] = [lsplit[1]]
 
     def get_data(self):
         files = glob.glob('./dataset/*/*.txt')
 
-        
+        for file in files:
+            self.get_dialogues(file)
+
+    @staticmethod
+    def get_json(filename, filedata):
+        with open(filename, 'w') as outfile:
+            json.dump(filedata, outfile, sort_keys=True, indent=4)
+
+        return os.path.abspath(os.path.dirname(filename))
+
+if __name__=="__main__":
+    p = Process()
+    p.get_data()
+    p.get_json('dilogue_data.json', p.dialogues)
+
+    for name in ["rick", "morty"]:
+        dial_ = []
+        for key, valuelist in p.dialogues.items():
+            if name in key.lower() and len(valuelist)>20:
+                dial_.append(" ".join(valuelist))
+
+        dial_ = " ".join(dial_)
+        dial_ = dial_.replace('\n', '')
+        p.get_json(name+'.json', {name: dial_})
